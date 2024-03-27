01270ec4fd5a703340e75de8bdc4e21a28cfca82
leiyiyan <69673191+leiyiyan@users.noreply.github.com>
2024-03-27 14:42:14 +0800

    更新"龙湖天街'

--- script/lhtj/lhtj.js
+++ script/lhtj/lhtj.js
@@ -244,26 +244,27 @@ async function getBalance(user) {
 //获取Cookie
 async function getCookie() {
   try {
-    if ($request && $request.method != 'OPTIONS'){
-      const header = ObjectKeys2LowerCase($request.headers);
-      // const body = $.toObj($request.body);
-      if (!(header.cookie)) throw new Error("获取Cookie错误，值为空");
-      const newData = {
-        "userName": '微信用户',
-        'x-lf-dxrisk-token': header['x-lf-dxrisk-token'],
-        "x-lf-channel": header['x-lf-channel'],
-        "token": header.token,
-        'x-lf-usertoken': header['x-lf-usertoken'],
-        "cookie": header.cookie,
-        "x-lf-bu-code": header['x-lf-bu-code'],
-        'x-lf-dxrisk-source': header['x-lf-dxrisk-source']
-      }
-      const index = userCookie.findIndex(e => e.token == newData.token);
-      index !== -1 ? userCookie[index] = newData : userCookie.push(newData);
-      $.setjson(userCookie, ckName);
-      $.msg($.name, `获取Cookie成功!`, ``)
-      
+    if ($request && $request.method === 'OPTIONS') throw new Error("Incorrect script execution method,only cron is permitted");
+  
+    const header = ObjectKeys2LowerCase($request.headers);
+    // const body = $.toObj($request.body);
+    if (!(header.cookie)) throw new Error("获取Cookie错误，值为空");
+    const newData = {
+      "userName": '微信用户',
+      'x-lf-dxrisk-token': header['x-lf-dxrisk-token'],
+      "x-lf-channel": header['x-lf-channel'],
+      "token": header.token,
+      'x-lf-usertoken': header['x-lf-usertoken'],
+      "cookie": header.cookie,
+      "x-lf-bu-code": header['x-lf-bu-code'],
+      'x-lf-dxrisk-source': header['x-lf-dxrisk-source']
     }
+    const index = userCookie.findIndex(e => e.token == newData.token);
+    index !== -1 ? userCookie[index] = newData : userCookie.push(newData);
+    $.setjson(userCookie, ckName);
+    $.msg($.name, `获取Cookie成功!`, ``)
+      
+    
 
   } catch (e) {
     throw e;
