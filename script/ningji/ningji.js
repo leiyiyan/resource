3dc783ee633e6f108b534cefdbbe3cb9c07181b5
leiyiyan <69673191+leiyiyan@users.noreply.github.com>
2024-10-03 19:28:37 +0800

    更新 ningji.js

--- script/ningji/ningji.js
+++ script/ningji/ningji.js
@@ -49,7 +49,7 @@ $.orgid = '429605'
 $.csecversionname = '67.44.000'
 $.csecversion = '1.4.0'
 $.messages = [];
-$.campaignId = "1006187217";
+$.campaignId = "1006191245";
 
 // 主函数
 async function main() {
